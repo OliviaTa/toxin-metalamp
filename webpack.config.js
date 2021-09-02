758fce8db32c787de5e724e0fe92255e98751f9e
OliviaTa <ir.klypina@gmail.com>
2021-09-02 20:32:06 +0800

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    src: path.resolve(__dirname, 'src'),
+    dist: path.resolve(__dirname, 'dist'),
+    assets: 'assets/'
+};
+
+const PAGES_DIR = `${PATHS.src}/pages`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.(s(a|c)ss)$/,
+                use: ['style-loader', 'css-loader', {
+                    loader: "sass-loader",
+                    options: {
+                        sourceMap: true,
+                        sassOptions: {
+                            outputStyle: "compressed",
+                        },
+                    },
+                },]
+            }
+        ]
+    },
+    plugins: [
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: path.resolve(PAGES_DIR, page),
+            filename: `./${page.replace(/\.pug/, '.html')}`
+        })),
+    ],
+    devServer: {
+        port: 3000,
+        hot: true,
+        open: true
+    },
+};
